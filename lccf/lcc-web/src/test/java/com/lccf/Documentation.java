3649c312a2389a6009c5734e41c4747ab1c46cc3
licc168 <liccwork@126.com>
2017-08-04 11:53:22 +0800

    生成api文档

--- lccf/lcc-web/src/test/java/com/lccf/Documentation.java
+++ lccf/lcc-web/src/test/java/com/lccf/Documentation.java
@@ -1,8 +1,12 @@
 package com.lccf;
 
-import io.github.robwin.markup.builder.MarkupLanguage;
-import io.github.robwin.swagger2markup.GroupBy;
-import io.github.robwin.swagger2markup.Swagger2MarkupConverter;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,8 +17,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import io.github.robwin.markup.builder.MarkupLanguage;
+import io.github.robwin.swagger2markup.GroupBy;
+import io.github.robwin.swagger2markup.Swagger2MarkupConverter;
 import springfox.documentation.staticdocs.SwaggerResultHandler;
 
 @AutoConfigureMockMvc
@@ -23,29 +30,40 @@ import springfox.documentation.staticdocs.SwaggerResultHandler;
 @SpringBootTest
 public class Documentation {
 
-    private String snippetDir = "target/generated-snippets";
-    private String outputDir = "target/asciidoc";
-    //private String indexDoc = "docs/asciidoc/index.adoc";
+    private String  snippetDir = "target/generated-snippets";
+    private String  outputDir  = "target/asciidoc";
+    // private String indexDoc = "docs/asciidoc/index.adoc";
 
     @Autowired
     private MockMvc mockMvc;
 
     @After
-    public void Test() throws Exception{
+    public void Test() throws Exception {
         // 得到swagger.json,写入outputDir目录中
-        mockMvc.perform(MockMvcRequestBuilders.get("/v2/api-docs").accept(MediaType.APPLICATION_JSON))
-                .andDo(SwaggerResultHandler.outputDirectory(outputDir).build())
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andReturn();
+        mockMvc.perform(get("/v2/api-docs").accept(APPLICATION_JSON)).andDo(SwaggerResultHandler.outputDirectory(outputDir).build())
+                .andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
 
         // 读取上一步生成的swagger.json转成asciiDoc,写入到outputDir
         // 这个outputDir必须和插件里面<generated></generated>标签配置一致
-        Swagger2MarkupConverter.from(outputDir + "/swagger.json")
-                .withPathsGroupedBy(GroupBy.TAGS)// 按tag排序
+        Swagger2MarkupConverter.from(outputDir + "/swagger.json").withPathsGroupedBy(GroupBy.TAGS)// 按tag排序
                 .withMarkupLanguage(MarkupLanguage.ASCIIDOC)// 格式
-                .withExamples(snippetDir)
-                .build()
-                .intoFolder(outputDir);// 输出
-    }
-}
+                .withExamples(snippetDir).build().intoFolder(outputDir);// 输出
 
+    }
+
+    @Test
+    public void testLogin() throws Exception {
+        mockMvc.perform(get("/login").param("userName", "admin").param("password", "111111").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk()).andDo(document("login", preprocessResponse(prettyPrint())));
+
+    }
+
+    @Test
+    public void TestApi() throws Exception {
+
+        mockMvc.perform(get("/api/menu/list").cookie().accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+                .andDo(document("list", preprocessResponse(prettyPrint())));
+
+    }
+
+}
