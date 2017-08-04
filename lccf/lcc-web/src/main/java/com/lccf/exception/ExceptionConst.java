3102d6db2c5cecfd41cfaef1aa511cd3641176f1
licc168 <liccwork@126.com>
2017-08-04 14:45:01 +0800

    集成log4j2

---
+++ lccf/lcc-web/src/main/java/com/lccf/exception/ExceptionConst.java
@@ -0,0 +1,15 @@
+package com.lccf.exception;
+
+/**
+ * 错误信息描述
+ *
+ * @author lichangchao
+ * @version 1.0.0
+ * @date 2017/8/3 16:39
+ * @see
+ */
+class ExceptionConst {
+    static final String USER_NOT_ACTIVATE = "用户未激活";
+    static final String USER_NOT_FOUND    = "用户名不存在";
+    static final String SYS_EXCEPTION     = "系统异常";
+}
